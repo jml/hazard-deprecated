f6c74e0f07afc1b15e143eb4ead2a2424541b063
Jonathan Lange <jml@mumak.net>
2015-05-09 16:34:45 +0100

    Don't duplicate shell.nix

--- shell.nix
+++ shell.nix
@@ -5,26 +5,4 @@ let modifiedHaskellPackages = haskellngPackages.override {
         hazard = self.callPackage ./. {};
       };
     };
-    pkg = modifiedHaskellPackages.callPackage
-            ({ mkDerivation, aeson, base, haverer, hspec-wai, hspec-wai-json
-             , http-types, scotty, stdenv, stm, tasty, tasty-hspec
-             , tasty-quickcheck, text, transformers
-             }:
-             mkDerivation {
-               pname = "hazard";
-               version = "0.1.0.0";
-               src = ./.;
-               isLibrary = true;
-               isExecutable = true;
-               buildDepends = [
-                 aeson base haverer http-types scotty stm text transformers
-               ];
-               testDepends = [
-                 aeson base hspec-wai hspec-wai-json scotty stm tasty tasty-hspec
-                 tasty-quickcheck
-               ];
-               description = "An HTTP API for playing Love Letter";
-               license = stdenv.lib.licenses.asl20;
-             }) {};
-in
-  pkg.env
+in modifiedHaskellPackages.hazard.env
