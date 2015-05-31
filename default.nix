06f4e83c64868f52ca5121986303023944bdfd21
Jonathan Lange <jml@mumak.net>
2015-05-31 14:07:30 +0100

    cabal2nix . > default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,8 @@
 { mkDerivation, aeson, base, base64-bytestring, basic-prelude
-, bytestring, containers, errors, haverer, hspec-wai, hspec-wai-json
-, http-types, lens, MonadRandom, mtl, random, Spock, stdenv, stm, tasty
-, tasty-hspec, tasty-quickcheck, text, transformers, wai, wai-extra
+, bytestring, containers, errors, haverer, hspec-wai
+, hspec-wai-json, http-types, lens, MonadRandom, mtl, random, Spock
+, stdenv, stm, tasty, tasty-hspec, tasty-quickcheck, text
+, transformers, wai, wai-extra
 }:
 mkDerivation {
   pname = "hazard";
@@ -10,13 +11,14 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   buildDepends = [
-    aeson base basic-prelude bytestring containers errors haverer http-types
-    lens MonadRandom mtl random Spock stm text transformers wai wai-extra
+    aeson base basic-prelude bytestring containers errors haverer
+    http-types lens MonadRandom mtl random Spock stm text transformers
+    wai wai-extra
   ];
   testDepends = [
-    aeson base basic-prelude base64-bytestring bytestring errors haverer
-    hspec-wai hspec-wai-json http-types MonadRandom random Spock stm tasty
-    tasty-hspec tasty-quickcheck text wai wai-extra
+    aeson base base64-bytestring basic-prelude bytestring errors
+    haverer hspec-wai hspec-wai-json http-types MonadRandom random
+    Spock stm tasty tasty-hspec tasty-quickcheck text wai wai-extra
   ];
   description = "An HTTP API for playing Love Letter";
   license = stdenv.lib.licenses.asl20;
